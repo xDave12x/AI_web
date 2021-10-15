c7f3228c9e543781914e82022281962638f2cf49
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-10-15 15:44:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+song="";
+song1="";
+function preload(){
+song=loadSound("music.mp3");
+song1=loadSound("music2.mp3");
+}
+function setup(){
+canvas=createCanvas(600,500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+image(video,0,0,600,500);
+}
